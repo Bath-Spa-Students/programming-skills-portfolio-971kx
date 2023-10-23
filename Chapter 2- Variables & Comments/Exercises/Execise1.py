0004ee5e9c0e3a5d349256de7b0b9c8517af9d23
971kx <a1701ahmad@gmail.com>
2023-10-23 16:42:45 +0400

    Chapter 2 Exercises

---
+++ Chapter 2- Variables & Comments/Exercises/Execise1.py
@@ -0,0 +1,12 @@
+## Exercise 1: Variables:ballot_box_with_check:
+
+# Assigning message to a variable 
+message = "My Name is Ahmad and i love coding"
+# Printing the message
+print(message)
+
+# Changing the message of the variable  
+message = "Coding is my passion"
+
+# Printing the message 
+print(message)
---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,10 @@
+## Exercise 2: Variables :ballot_box_with_check:
+
+# Famous person I admire 
+author = "Elon Musk"
+
+# Elon musk quotation
+quote = "When something is important enough, you do it even if the odds are not in your favor."
+
+# Printing the quote along with its author
+print(f'{author} once said, "{quote}"')
