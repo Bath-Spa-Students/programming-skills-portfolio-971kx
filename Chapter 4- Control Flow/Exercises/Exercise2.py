6ac4a747e5e968bdea4e0a59a5779dc6b17983b3
971kx <144207048+971kx@users.noreply.github.com>
2023-11-08 09:44:03 +0400

    Update Exercise2.py

--- Chapter 4- Control Flow/Exercises/Exercise2.py
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -1,1 +1,20 @@
+# Choosing the color of the alien 
+alien_color = 'red'
 
+# Verifying the alien's color is red
+if alien_color == 'red':
+    print("Congratulations! You just earned 5 points for shooting the red alien.")
+else:
+    print("Congratulations! You just earned 10 points for shooting a non-red alien.")
+
+
+
+
+# Choosing the color of the alien 
+alien_color = 'red'
+
+# Verifying the alien's color is red
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+else:
+    print("Congratulations! You just earned 10 points for shooting a non-green alien.")
