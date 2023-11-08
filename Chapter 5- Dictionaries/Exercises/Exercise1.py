0ea364b012594a527ff1e71a336b0122e87da22f
971kx <144207048+971kx@users.noreply.github.com>
2023-11-08 09:48:04 +0400

    Create Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,13 @@
+# Making a dictionary to store information of a friend
+person_info = {
+    'first_name': 'Syed',
+    'last_name': 'Abdul Rafay',
+    'age': 18,
+    'city': 'Al Nahda'
+}
+
+# Displaying all the info stored in the dictionary
+print('First Name: ' + person_info['first_name'])
+print('Last Name: ' + person_info['last_name'])
+print('Age: ' + str(person_info['age']))  # Converting age to string for displaying
+print('City: ' + person_info['city'])
