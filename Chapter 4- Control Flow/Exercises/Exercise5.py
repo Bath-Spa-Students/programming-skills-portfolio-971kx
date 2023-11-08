9bb51953f65671b806408e5ae83b59f18b4aebb8
971kx <144207048+971kx@users.noreply.github.com>
2023-11-08 09:46:52 +0400

    Create Exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,14 @@
+#List of my 3 favourite fruits
+favorite_fruits = ['Apricot', 'Dragon fruit', 'Avocado']
+
+#Checking whether a certain kind of fruit is in my list or not
+if 'Kiwi' in favorite_fruits:
+    print("Ahmad loves kiwi!")
+if 'Apricot' in favorite_fruits:
+    print("Ahmad loves Apricot!")
+if 'Raspberry' in favorite_fruits:
+    print("Ahmad loves Raspberry!")   
+if 'Dragon fruit' in favorite_fruits:
+    print('Ahmad loves Dragon fruit!')
+if 'Avocado' in favorite_fruits:
+    print("Ahmad loves Avocado!")
