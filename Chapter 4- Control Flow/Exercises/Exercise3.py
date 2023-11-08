df0944189f8feba4238a86949467c4d92661603e
971kx <144207048+971kx@users.noreply.github.com>
2023-11-08 09:45:26 +0400

    Create Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,38 @@
+# Choosing the color of the alien 
+alien_color = 'green'
+
+# Verfiying if the alien's color is green
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+elif alien_color == 'yellow':
+    print("Congratulations! You just earned 10 points for shooting the yellow alien.")
+else:
+    print("Congratulations! You just earned 15 points for shooting the red alien.")
+
+
+# 2nd Ver. Assigning 10 points for shooting the yellow Alien 
+
+# Choosing the color of the alien 
+alien_color = 'yellow'
+
+# Verifying if the alien's color is yellow
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+elif alien_color == 'yellow':
+    print("Congratulations! You just earned 10 points for shooting the yellow alien.")
+else:
+    print("Congratulations! You just earned 15 points for shooting the red alien.")
+
+
+
+
+# Choosing the color of the alien 
+alien_color = 'red'
+
+# Verifying if the alien's color is red
+if alien_color == 'green':
+    print("Congratulations! You just earned 5 points for shooting the green alien.")
+elif alien_color == 'yellow':
+    print("Congratulations! You just earned 10 points for shooting the yellow alien.")
+else:
+    print("Congratulations! You just earned 15 points for shooting the red alien.")
