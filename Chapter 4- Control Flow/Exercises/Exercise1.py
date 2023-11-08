a9b36fa6d797361c80a39da52c54434b17c7b974
971kx <144207048+971kx@users.noreply.github.com>
2023-11-08 09:43:16 +0400

    Update Exercise1.py

--- Chapter 4- Control Flow/Exercises/Exercise1.py
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -1,1 +1,14 @@
+#Choosing Alien Color (Green)
+alien_color = "green"
 
+#if statement
+if alien_color == "green" :
+    print("Congratulations! You just earned 5 points.")
+
+
+#Choosing Alien Color (red)
+alien_color = "red"
+
+#if statement
+if alien_color == "green" :
+    print("Congratulations! You just earned 5 points.")
