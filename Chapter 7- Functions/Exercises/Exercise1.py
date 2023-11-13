de834810f6936adfd60ab603cf81ead56681b254
971kx <144207048+971kx@users.noreply.github.com>
2023-11-14 00:49:55 +0400

    Create Exercise1.py

---
+++ Chapter 7- Functions/Exercises/Exercise1.py
@@ -0,0 +1,8 @@
+def display_message(topic="functions"):
+    print(f"In this chapter, I am learning about functions in Python.")
+
+# Call the function to display the message with the default topic
+display_message()
+
+# Call the function with a different topic
+display_message("loops")
