1e22ccf73318b482d43d79c90c0c36486bf0d130
971kx <144207048+971kx@users.noreply.github.com>
2023-11-14 00:50:21 +0400

    Create Exercise2.py

---
+++ Chapter 7- Functions/Exercises/Exercise2.py
@@ -0,0 +1,6 @@
+def favorite_book():
+    title = input("Enter the title of your favorite book: ")
+    print(f"One of my favorite books is {title}.")
+
+# Call the function to prompt the user for a book title
+favorite_book()
