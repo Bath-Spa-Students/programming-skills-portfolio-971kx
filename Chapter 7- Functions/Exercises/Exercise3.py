5b0389fc0cb3d84d1955153acb46ecb3c971c80e
971kx <144207048+971kx@users.noreply.github.com>
2023-11-14 00:50:51 +0400

    Create Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,8 @@
+def make_shirt(size="L", message="I love Python"):
+    print(f"Making a shirt of size {size}. Message on the shirt: '{message}'.")
+
+# Call the function with default values
+make_shirt()
+
+# Call the function with different values
+make_shirt(size="M", message="Keep Calm and Code On")
